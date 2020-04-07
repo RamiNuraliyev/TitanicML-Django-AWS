969e5d0021666662d15b35d48370e69c5845aa87
RamiNuraliyev <rami.nuraliyav@gmail.com>
2020-04-07 15:57:39 +0300

    Uploading all Django files, including python file for training model

---
+++ titanicProject/titanicProject/ml_model.py
@@ -0,0 +1,15 @@
+
+
+
+def prediction_model(pclass, sex, age, sibsp, parch, fare, embarked, title):
+    import pickle
+    x= [[pclass, sex, age, sibsp, parch, fare, embarked, title]]
+    randomforest = pickle.load(open('titanic_model.sav','rb'))
+    prediction = randomforest.predict(x)
+    if prediction == 0:
+        return "Not survived"
+    elif prediction == 1:
+        return "Survived"
+    else:
+        prediction = "Eror"
+    return prediction
